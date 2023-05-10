866b6599bb42b7010da3c0a28931b02ccd1e14f6
Snehit Sah <snehitsah@protonmail.com>
2023-05-10 16:19:28 +0530

    test: cli config parse
    Signed-off-by: Snehit Sah <snehitsah@protonmail.com>

--- src/cli.rs
+++ src/cli.rs
@@ -22,3 +22,37 @@ pub fn config_path(config_arg: Option<&String>) -> Result<PathBuf, Box<dyn Error
 
     Ok(config_path)
 }
+
+#[cfg(test)]
+mod tests {
+    use std::path::PathBuf;
+
+    use crate::cli::{config_path, default_config_path};
+
+    #[test]
+    fn default_config() {
+        assert_eq!(config_path(None).unwrap(), default_config_path());
+    }
+
+    #[test]
+    fn custom_config() {
+        assert_eq!(
+            config_path(Some(&String::from(
+                "/home/user/.config/my-custom-config.toml"
+            )))
+            .unwrap(),
+            PathBuf::from("/home/user/.config/my-custom-config.toml")
+        );
+    }
+
+    #[test]
+    fn custom_config_accented() {
+        assert_eq!(
+            config_path(Some(&String::from(
+                "/home/àèìòùÀÈÌÒÙáéíóúýÁÉÍÓÚÝâêîôûÂÊÎÔÛãñõÃÑÕäëïöüÿÄËÏÖÜŸ/.config/my-custom-config.toml"
+            )))
+            .unwrap(),
+            PathBuf::from("/home/àèìòùÀÈÌÒÙáéíóúýÁÉÍÓÚÝâêîôûÂÊÎÔÛãñõÃÑÕäëïöüÿÄËÏÖÜŸ/.config/my-custom-config.toml")
+        );
+    }
+}
